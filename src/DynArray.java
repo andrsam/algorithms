0c30ce314555126b2ba9608af9be9c5f2586b601
Andrey Samoylov <andrsbox@gmail.com>
2019-06-14 19:55:57 +0300

    Merge branch 'master' of C:\IdeaProjects\algorithms with conflicts.

--- src/DynArray.java
+++ src/DynArray.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class DynArray<T> {
-    public final int MIN_CAPACITY = 16;
+    public static final int MIN_CAPACITY = 16;
     public T[] array;
     public int count;
     public int capacity;
@@ -27,46 +27,36 @@ public class DynArray<T> {
     }
 
     public T getItem(int index) {
-        checkIndex(index);
+        if (index < 0 || index > array.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
         return array[index];
     }
 
     public void append(T itm) {
-        insert(itm, count == 0 ? 0 : count - 1);
+        ensureCapacity();
+        array[count++] = itm;
     }
 
     public void insert(T itm, int index) {
-        checkIndex(index);
-        if (count == capacity) {
+        ensureCapacity();
+        if (index == count - 1) {
+            append(itm);
+        }  else {
+            System.arraycopy(array, index + 1, array, index + 2, count - index - 1);
+            array[index] = itm;
+        }
+    }
+
+    private void ensureCapacity() {
+        if (count + 1 > capacity) {
             makeArray(capacity * 2);
         }
-
-        if (index < count - 1) {
-            System.arraycopy(array, index, array, index + 1, count - index);
-        }
-        array[index] = itm;
-        count++;
     }
 
     public void remove(int index) {
-        if (count == 0) {
-            return;
-        }
-        checkIndex(index);
-        System.arraycopy(array, index + 1, array, index, count - index - 1);
-        count--;
-
-        if (capacity > MIN_CAPACITY && count < capacity / 2) {
-            capacity = (capacity / 1.5 < MIN_CAPACITY) ? MIN_CAPACITY : (int) (capacity / 1.5);
-            makeArray(capacity);
-        }
-
-    }
-
-    private void checkIndex(int index) {
-        if (index < 0 || index > capacity - 1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+        // ваш код
     }
 
 }
--- src/DynArrayTest.java
+++ src/DynArrayTest.java
@@ -1,74 +1,45 @@
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-@SuppressWarnings("unchecked")
 public class DynArrayTest {
     private DynArray dynArray = new DynArray(Integer.class);
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
     @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void getItemWrongPosition() {
-        dynArray.getItem(-1);
-        dynArray.getItem(16);
+    public void getItem() throws ArrayIndexOutOfBoundsException {
+       dynArray.array[0] = 1;
+       assertEquals(1, dynArray.getItem(0));
+       dynArray.getItem(DynArray.MIN_CAPACITY+1);
+       thrown.expect(ArrayIndexOutOfBoundsException.class);
+       dynArray.getItem(-1);
+       thrown.expect(ArrayIndexOutOfBoundsException.class);
     }
 
     @Test
     public void append() {
         dynArray.append(1);
         assertEquals(1, dynArray.getItem(0));
-        assertEquals(1, dynArray.count);
-        assertEquals(dynArray.MIN_CAPACITY, dynArray.capacity);
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void insertToWrongPosition() {
-        fillArray(8);
-        dynArray.insert(32, -1);
     }
 
     @Test
     public void insert() {
-        fillArray(8);
-        dynArray.insert(32, 1);
-        assertArrayEquals(new Integer[]{1, 32, 1, 1, 1, 1, 1, 1}, Arrays.copyOfRange(dynArray.array, 0, 8));
-        assertEquals(9, dynArray.count);
-        assertEquals(dynArray.MIN_CAPACITY, dynArray.capacity);
-        fillArray(16);
-        dynArray.insert(32, 1);
-        assertEquals(32, dynArray.getItem(1));
-        assertEquals(32, dynArray.capacity);
-        assertEquals(17, dynArray.count);
+        for (int i = 0; i < 7; i++) {
+            dynArray.append(1);
+        }
+        dynArray.insert(5, 1);
+        assertEquals(5, dynArray.getItem(1));
     }
-
 
     @Test
     public void remove() {
-        dynArray.remove(0);
-        assertEquals(0, dynArray.count);
-        fillArray(8);
-        dynArray.remove(1);
-        assertEquals(7, dynArray.count);
-        dynArray.makeArray(32);
-        fillArray(16);
-        dynArray.remove(1);
-        assertEquals(21, dynArray.capacity);
-        while (dynArray.count > 9) {
-            dynArray.remove(0);
-        }
-        assertEquals(dynArray.MIN_CAPACITY, dynArray.capacity);
     }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void removeFromWrongPosition() {
-        dynArray.remove(-1);
-    }
-
-    private void fillArray(int length) {
-        Arrays.fill(dynArray.array, 0, length, 1);
-        dynArray.count = length;
-    }
-
 }
