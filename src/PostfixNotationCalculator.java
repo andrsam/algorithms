a632045a2e09bec2a8e4cc3d5c0540a5bc08e588
Andrey Samoylov <andrsbox@gmail.com>
2019-06-26 10:12:14 +0300

    fix evaluating non-valid expressions

--- src/PostfixNotationCalculator.java
+++ src/PostfixNotationCalculator.java
@@ -1,9 +1,10 @@
 @SuppressWarnings("unchecked")
 public class PostfixNotationCalculator {
-    private Stack expressionStack = new Stack();
-    private Stack operandsStack = new Stack();
 
     public int computeExpression(String expression) {
+        Stack expressionStack = new Stack();
+        Stack operandsStack = new Stack();
+
         String[] arr = expression.split(" ");
 
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -14,11 +15,14 @@ public class PostfixNotationCalculator {
         String elem = "";
         while (expressionStack.size() > 0 && !elem.equals("=")) {
             elem = expressionStack.pop().toString();
-            if (!(elem.equals("+") || elem.equals("*"))) {
+
+            if (elem.matches("[0-9]+")) {
                 operandsStack.push(elem);
-            } else {
+            } else if (operandsStack.size() >= 2) {
                 result = calcOperation(operandsStack.pop().toString(), operandsStack.pop().toString(), elem);
                 operandsStack.push(result);
+            } else if (operandsStack.size() == 1) {
+                result = Integer.parseInt(operandsStack.pop().toString());
             }
         }
 
@@ -40,5 +44,9 @@ public class PostfixNotationCalculator {
                 break;
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("+".matches("[0-9]+|\\+|\\*"));
     }
 }
