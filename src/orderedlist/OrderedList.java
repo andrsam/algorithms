fd36ee1617abd5fb703f63f9f238806357c73afe
Andrey Samoylov <andrsbox@gmail.com>
2019-07-26 09:19:25 +0300

    ordered list

---
+++ src/orderedlist/OrderedList.java
@@ -0,0 +1,162 @@
+package orderedlist;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+class Node<T> {
+    public T value;
+    public Node<T> next, prev;
+
+    public Node(T _value) {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(value, node.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
+
+public class OrderedList<T> {
+    public Node<T> head, tail;
+    private boolean _ascending;
+
+    private static final String DATATYPE_STRING = "java.lang.String";
+    private static final String DATATYPE_INTEGER = "java.lang.Integer";
+
+    public OrderedList(boolean asc) {
+        head = null;
+        tail = null;
+        _ascending = asc;
+    }
+
+    public int compare(T v1, T v2) {
+        // -1 если v1 < v2
+        // 0 если v1 == v2
+        // +1 если v1 > v2
+        if (v1 == null) {
+            return -1;
+        }
+        int cmp = 0;
+        switch (v1.getClass().getName()) {
+            case DATATYPE_STRING:
+                cmp = ((String) v1).trim().compareTo(((String) v2).trim());
+                break;
+            case DATATYPE_INTEGER:
+                cmp = ((Integer) v1).compareTo((Integer) v2);
+                break;
+        }
+        return cmp;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void add(T value) {
+        // автоматическая вставка value
+        // в нужную позицию
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            this.head = newNode;
+            this.tail = this.head;
+            this.head.next = null;
+            this.head.prev = null;
+        } else {
+            Node<T> prevNode = head;
+            while (prevNode.next != null && compare(prevNode.value, value) == getSortOrder()) {
+                prevNode = prevNode.next;
+            }
+            if (_ascending) {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                tail.prev = newNode;
+                tail = head;
+                head = newNode;
+            }
+        }
+
+    }
+
+    public Node<T> find(T val) {
+        if (head == null) {
+            return null;
+        }
+        Node node = head;
+        while (node.value != val) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void delete(T val) {
+        Node node = this.head;
+
+        while (node != null) {
+            if (node.value == val) {
+                break;
+            }
+            node = node.next;
+        }
+
+        if (node == null) {
+            return;
+        }
+
+        if (node == this.head) {
+            this.head = node.next;
+            if (this.head == null) {
+                this.tail = null;
+            } else {
+                this.head.prev = null;
+            }
+        } else if (node == this.tail) {
+            this.tail = node.prev;
+            this.tail.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+    }
+
+    public void clear(boolean asc) {
+        _ascending = asc;
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int count() {
+        Node node = this.head;
+        int count;
+        for (count = 0; node != null; count++, node = node.next) ;
+        return count;
+    }
+
+    ArrayList<Node<T>> getAll() {
+        // выдать все элементы упорядоченного
+        // списка в виде стандартного списка
+        ArrayList<Node<T>> r = new ArrayList<>();
+        Node<T> node = head;
+        while (node != null) {
+            r.add(node);
+            node = node.next;
+        }
+        return r;
+    }
+
+    private int getSortOrder() {
+        return _ascending ? -1 : 1;
+    }
+}
---
+++ src/orderedlist/OrderedListTest.java
@@ -0,0 +1,84 @@
+package orderedlist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class OrderedListTest {
+    private OrderedList<Integer> orderedListIntAsc = new OrderedList<>(true);
+    private OrderedList<Integer> orderedListIntDesc = new OrderedList(false);
+
+    private OrderedList<String> orderedListStrAsc = new OrderedList<>(true);
+    private OrderedList<String> orderedListStrDesc = new OrderedList(false);
+
+    @Test
+    public void add() {
+        orderedListIntAsc.add(1);
+        orderedListIntAsc.add(2);
+        assertEquals(2, orderedListIntAsc.count());
+        assertEquals(1, orderedListIntAsc.head.value.intValue());
+        assertEquals(2, orderedListIntAsc.head.next.value.intValue());
+        assertEquals(2, orderedListIntAsc.tail.value.intValue());
+        assertEquals(1, orderedListIntAsc.tail.prev.value.intValue());
+        orderedListIntAsc.add(3);
+        assertEquals(3, orderedListIntAsc.count());
+        assertEquals(1, orderedListIntAsc.head.value.intValue());
+        assertEquals(3, orderedListIntAsc.tail.value.intValue());
+
+        orderedListIntDesc.add(1);
+        orderedListIntDesc.add(2);
+        assertEquals(2, orderedListIntDesc.count());
+        assertEquals(2, orderedListIntDesc.head.value.intValue());
+        assertEquals(1, orderedListIntDesc.head.next.value.intValue());
+        assertEquals(1, orderedListIntDesc.tail.value.intValue());
+        assertEquals(2, orderedListIntDesc.tail.prev.value.intValue());
+
+        orderedListIntDesc.add(-1);
+        orderedListIntDesc.add(-2);
+        orderedListIntDesc.add(9);
+        assertEquals(5, orderedListIntDesc.count());
+        assertEquals(9, orderedListIntDesc.head.value.intValue());
+        assertEquals(-2, orderedListIntDesc.tail.value.intValue());
+
+        orderedListStrAsc.add("a");
+        orderedListStrAsc.add("b");
+        assertEquals(2, orderedListStrAsc.count());
+        assertEquals("a", orderedListStrAsc.head.value);
+        assertEquals("b", orderedListStrAsc.head.next.value);
+        assertEquals("b", orderedListStrAsc.tail.value);
+        assertEquals("a", orderedListStrAsc.tail.prev.value);
+
+        orderedListStrDesc.add("a");
+        orderedListStrDesc.add("b");
+        assertEquals(2, orderedListStrDesc.count());
+        assertEquals("b", orderedListStrDesc.head.value);
+        assertEquals("a", orderedListStrDesc.head.next.value);
+        assertEquals("a", orderedListStrDesc.tail.value);
+        assertEquals("b", orderedListStrDesc.tail.prev.value);
+    }
+
+    @Test
+    public void find() {
+        assertNull(orderedListIntAsc.find(3));
+        orderedListIntAsc.add(1);
+        orderedListIntAsc.add(2);
+        orderedListIntAsc.add(3);
+        assertEquals(2, orderedListIntAsc.find(2).value.intValue());
+    }
+
+    @Test
+    public void delete() {
+        orderedListIntAsc.add(1);
+        orderedListIntAsc.add(2);
+        orderedListIntAsc.add(3);
+        orderedListIntAsc.add(4);
+
+        orderedListIntAsc.delete(1);
+        assertEquals(2, orderedListIntAsc.head.value.intValue());
+        assertEquals(4, orderedListIntAsc.tail.value.intValue());
+
+        orderedListIntAsc.delete(4);
+        assertEquals(3, orderedListIntAsc.tail.value.intValue());
+    }
+}
