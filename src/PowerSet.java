59415bbf85f9c0dd4bf033218c045f1366fca177
Andrey Samoylov <andrsbox@gmail.com>
2019-08-22 14:14:47 +0300

    power set

---
+++ src/PowerSet.java
@@ -0,0 +1,156 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PowerSet<T> {
+    public int size;
+    public ArrayList<T> slots;
+    public Class<T> clazz;
+
+
+    @SuppressWarnings("unchecked")
+    public PowerSet(int sz, Class<T> clazz) {
+        // ваша реализация хранилища
+        this.size = sz;
+        this.clazz = clazz;
+        slots = new ArrayList<>(Arrays.asList((T[]) Array.newInstance(clazz, sz)));
+    }
+
+    private int hashFun(T value) {
+        // всегда возвращает корректный индекс слота
+        if (clazz == Integer.class) {
+            return (Integer) value;
+        }
+        String valStr = value.toString();
+        int sum = 0;
+        for (char chr : valStr.toCharArray()) {
+            sum += chr;
+        }
+        return sum % size;
+    }
+
+    public int size() {
+        // количество элементов в множестве
+        return asList().size();
+    }
+
+
+    public List<T> asList() {
+        return slots.stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public void put(T value) {
+        // всегда срабатывает
+        if (value == null) {
+            return;
+        }
+        int i = hashFun(value);
+        T slotVal = slots.get(i);
+        if (slotVal == null || (!slotVal.equals(value))) {
+            slots.set(i, value);
+        }
+    }
+
+    public boolean get(T value) {
+        // возвращает true если value имеется в множестве,
+        // иначе false
+        int i = hashFun(value);
+        return slots.get(i) != null && slots.get(i).equals(value);
+    }
+
+    public boolean remove(T value) {
+        // возвращает true если value удалено
+        // иначе false
+        if (get(value)) {
+            slots.set(hashFun(value), null);
+            return true;
+        }
+        return false;
+    }
+
+    public PowerSet<T> intersection(PowerSet<T> set2) {
+        // пересечение текущего множества и set2
+        PowerSet<T> result = new PowerSet<>(this.size, this.clazz);
+
+        for (int i = 0; i < size; i++) {
+            if (slots.get(i) != null && set2.get(slots.get(i))) {
+                result.put(slots.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public PowerSet<T> union(PowerSet<T> set2) {
+        // объединение текущего множества и set2
+        PowerSet<T> result = new PowerSet<>(this.size, this.clazz);
+        for (int i = 0; i < size; i++) {
+            if (this.slots.get(i) != null) {
+                result.put(this.slots.get(i));
+            }
+        }
+
+        for (int i = 0; i < set2.size; i++) {
+            if (set2.slots.get(i) != null) {
+                result.put(set2.slots.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public PowerSet<T> difference(PowerSet<T> set2) {
+        // разница текущего множества и set2
+        PowerSet<T> result = new PowerSet<>(this.size, clazz);
+
+        for (int i = 0; i < size; i++) {
+            if (slots.get(i) != null) {
+                if (!set2.get(slots.get(i))) {
+                    result.put(slots.get(i));
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public boolean isSubset(PowerSet<T> set2) {
+        // возвращает true, если set2 есть
+        // подмножество текущего множества,
+        // иначе false
+        boolean result = true;
+
+        List<T> nonNullslots = set2.slots.stream().filter(Objects::nonNull).collect(Collectors.toList());
+
+        for (int i = 0; i < nonNullslots.size(); i++) {
+            result = result && get(nonNullslots.get(i));
+        }
+
+        return result;
+    }
+
+    public void fillFromList(List<T> list) {
+        slots = new ArrayList<>(Arrays.asList((T[]) Array.newInstance(clazz, size)));
+        for (int i = 0; i < list.size(); i++) {
+            put(list.get(i));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PowerSet<?> powerSet = (PowerSet<?>) o;
+        return size == powerSet.size &&
+                Objects.equals(slots, powerSet.slots);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, slots);
+    }
+
+}
---
+++ src/PowerSetTest.java
@@ -0,0 +1,73 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class PowerSetTest {
+    PowerSet<Integer> set1 = new PowerSet<>(20000, Integer.class);
+    PowerSet<Integer> set2 = new PowerSet<>(20000, Integer.class);
+
+    @Test
+    public void intersection() {
+        assertEquals(0, set1.intersection(set2).size());
+
+        set1.fillFromList(Arrays.asList(1, 2, 3, 4, 7));
+        set2.fillFromList(Arrays.asList(2, 3, 5, 7));
+        assertEquals(Arrays.asList(2, 3, 7), set1.intersection(set2).asList());
+
+        set1.fillFromList(Arrays.asList(1, 3, 5, 7));
+        set2.fillFromList(Arrays.asList(2, 4, 6, 8));
+        assertTrue(set1.intersection(set2).asList().isEmpty());
+
+    }
+
+    @Test
+    public void union() {
+        assertEquals(0, set1.union(set2).size());
+
+        set1.fillFromList(Arrays.asList(1, 2, 3, 4, 7));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 7), set1.union(set2).asList());
+
+        set2.fillFromList(Arrays.asList(2, 3, 5, 7));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 7), set1.union(set2).asList());
+    }
+
+    @Test
+    public void difference() {
+        assertEquals(0, set1.difference(set2).size());
+
+        set1.fillFromList(Arrays.asList(1, 2, 3, 4, 7));
+        assertEquals(0, set1.difference(set1).size());
+
+        set2.fillFromList(Arrays.asList(2, 3, 5, 7));
+        assertEquals(Arrays.asList(1, 4), set1.difference(set2).asList());
+    }
+
+    /**
+     * - issubset(), в качестве параметра выступает другое множество, и проверяется,
+     * входят ли все его элементы в текущее множество (будет ли множество-параметр подмножеством текущего множества).
+     */
+
+
+    @Test
+    public void isSubset() {
+        //рассмотрите три случая (все элементы параметра входят в текущее множество,
+        // все элементы текущего множества входят в параметр, не все элементы параметра входят в текущее множество).
+
+        set1.fillFromList(Arrays.asList(2, 3, 5));
+        set2.fillFromList(Arrays.asList(2, 3, 5, 7));
+        assertTrue(set2.isSubset(set1));
+        assertFalse(set1.isSubset(set2));
+
+        set1.fillFromList(Arrays.asList(2, 3, 5));
+        set2.fillFromList(Arrays.asList(2, 3, 5));
+        assertTrue(set1.isSubset(set2));
+
+
+    }
+
+    @Test
+    public void remove() {
+    }
+}
